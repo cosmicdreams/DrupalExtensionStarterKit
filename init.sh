a1ff41ee8ba835ad2f09c1ab45a5d7fed3fd99d8
Chris Weber <cweber@nerdery.com>
2015-12-30 10:57:43 -0600

    Added an extra condition + comments
    To allow this executable to be run multiple times without extra noise of unitary changes, I added a condition around the action that gets composer.  And I added comments because I can't help myself.

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
+# If composer isn't installed globally, get a local version to use.
+if [ ! hash composer 2>/dev/null ]; then
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install
+fi
+
+# Get a selenium executable if you don't already have a local copy.
 if [ ! -d "bin/selenium" ]; then
   mkdir -p bin/selenium
   bash selenium.sh update
